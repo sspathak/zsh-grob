34b411364a4e64ec89d9d2a6fc40e8ec13ab7ce5
sspathak <suraj.s.pathak@gmail.com>
2026-01-08 09:47:46 -0500

    added SHA for rb

--- git-grob.rb
+++ git-grob.rb
@@ -2,7 +2,7 @@ class GitGrob < Formula
   desc "Interactive git rebase --onto helper for managing stacked PRs"
   homepage "https://github.com/sspathak/zsh-grob"
   url "https://github.com/sspathak/zsh-grob/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "" # Will be filled in when creating actual release
+  sha256 "e1921172d39768a24229e6113971381cbdbabf3c5f3f557bd4ba23eac7f5873a" # Will be filled in when creating actual release
   license "MIT"
 
   depends_on "fzf"
